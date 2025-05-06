2c366980b55ce939be026feb5651597d7e002da6
samilturksoy <samiltuksoy@gmail.com>
2025-05-06 18:35:01 +0300

    Implement category fetching in App component and enhance API module
    - Added useEffect to fetch categories from the API on component mount.
    - Updated App component to display "Meal App" instead of "App".
    - Introduced fetchCategories function in the API module to retrieve categories using axios.
    - Added error handling for API requests in fetchCategories and fetchMealsByCategories functions.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { fetchCategories } from './src/api/api';
 
 const App = () => {
+  useEffect(() => {
+
+    const getCategories = async ()=>{
+      const categories = await fetchCategories();
+      console.log(JSON.stringify(categories, null, 2) );
+    };
+    getCategories();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>App</Text>
-    </SafeAreaView>
+    <View>
+      <Text>Meal App</Text>
+    </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-
-});
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,2 +1,25 @@
+import axios from 'axios';
+
 const API_URL = 'https://www.themealdb.com/api/json/v1/1/';
+
+export const fetchCategories = async ()=>{
+    try {
+        const response = await axios.get(`${API_URL}categories.php`);
+        return response.data.categories;
+
+    } catch (error) {
+        console.error('Kategoriler çekilemedi',error);
+        return [];
+    }
+};
+
+export const fetchMealsByCategories = async (category:string)=>{
+    try {
+        const response = await axios.get(`${API_URL}filter.php?${category}`);
+        return response.data.meals;
+    } catch (error) {
+        console.error(`${category} kategorisi için yemekler alınamadı`,error);
+        return [];
+    }
+};
 export default API_URL;
